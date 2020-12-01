25450d579e4ddc536984c6a305422dff5ac659d2
Rafael Falcão <rafaelfalcaof@gmail.com>
2020-12-01 15:12:04 -0300

    Initial Commit

---
+++ TechChallenge/CheckoutValidator/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Technical Challenge
+ *
+ * @category    TechChallenge
+ * @package     TechChallenge_CheckoutValidator
+ * @author      Rafael Falcão <rafaelfalcaof@gmail.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'TechChallenge_CheckoutValidator',
+    __DIR__
+);
